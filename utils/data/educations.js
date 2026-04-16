4eb090e0f8bc55bbf5a4f19900f73543abc41417
Desmond Ndunda <ndundadesmond@gmail.com>
2026-04-17 00:07:30 +0300

    Update education dates for KCA and Incepter

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,13 +2,13 @@ export const educations = [
   {
     id: 1,
     title: "Bachelor Degree Information Technology",
-    duration: "2021 - Present",
+    duration: "2024 - 2026",
     institution: "KCA University, Nairobi, Kenya",
   },
   {
     id: 2,
     title: "incepter institute of science and technology",
-    duration: "2018 - 2020",
+    duration: "2023 - 2025",
     institution: "cyber security and ethical hacking",
   }
 ]
